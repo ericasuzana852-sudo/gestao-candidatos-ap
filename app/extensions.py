2b637544a5d0e212878007f3c418caf5476c79fb
ericasuzana852-sudo <ericasuzana852@gmail.com>
2026-04-24 16:54:41 -0300

    Create extensions.py

---
+++ app/extensions.py
@@ -0,0 +1,10 @@
+"""Inicializacao das extensoes Flask (importadas em app/__init__.py)."""
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_wtf import CSRFProtect
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+migrate = Migrate()
+csrf = CSRFProtect()
